60ed76cf8f6381f47f8069dbf1dcb2a46bfef58e
Ramazan Onur Abbasoğlu <ramonur26@gmail.com>
2023-03-14 17:42:52 +0300

    Login process is complete.

--- Presentation/Controllers/LoginController.cs
+++ Presentation/Controllers/LoginController.cs
@@ -1,16 +1,46 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using DataAccess.Concrete;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Writer writer)
+        {
+            Context c = new Context();
+            var dataValue = c.Writers.FirstOrDefault(x => x.WriterMail == writer.WriterMail && x.WriterPassword == writer.WriterPassword);
+            if (dataValue != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, writer.WriterMail)
+                };
+                var userIdentity = new ClaimsIdentity(claims, "a");
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
--- Presentation/Startup.cs
+++ Presentation/Startup.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -17,7 +20,21 @@ namespace Presentation
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSession();
             services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/login/Index";
+                }
+                );
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,8 +47,11 @@ namespace Presentation
 
             app.UseStatusCodePagesWithReExecute("/ErrorPage/Error", "?code={0}");
 
+            app.UseSession();
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
