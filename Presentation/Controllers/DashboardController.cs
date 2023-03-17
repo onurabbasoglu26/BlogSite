a211e00ba1b98c53af60738f3ea229b130e4381c
Ramazan Onur Abbasoğlu <ramonur26@gmail.com>
2023-03-17 17:44:00 +0300

    Dashboard  completed.

--- Presentation/Controllers/DashboardController.cs
+++ Presentation/Controllers/DashboardController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,8 +12,14 @@ namespace Presentation.Controllers
     [AllowAnonymous]
     public class DashboardController : Controller
     {
+        readonly BlogManager blogManager = new BlogManager(new EfBlogDal());
+        readonly CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
         public IActionResult Index()
         {
+            ViewBag.Statistic1 = blogManager.GetList().Count();
+            ViewBag.Statistic2 = blogManager.GetList().Where(x => x.WriterId == 1).Count();
+            ViewBag.Statistic3 = categoryManager.GetList().Count();
             return View();
         }
     }
