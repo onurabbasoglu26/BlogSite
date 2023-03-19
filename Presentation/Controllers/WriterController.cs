99e919f3e4cf90122287e0f930f4bef3ffaf30ac
Ramazan Onur Abbasoğlu <ramonur26@gmail.com>
2023-03-19 17:05:14 +0300

    Writer edit profile page completed.

--- Presentation/Controllers/WriterController.cs
+++ Presentation/Controllers/WriterController.cs
@@ -1,3 +1,8 @@
+using Business.Concrete;
+using Business.ValidationRules;
+using DataAccess.Concrete.EntityFramework;
+using Entity.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,6 +11,8 @@ namespace Presentation.Controllers
     [AllowAnonymous]
     public class WriterController : Controller
     {
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
+
         public IActionResult Index()
         {
             return View();
@@ -24,6 +31,43 @@ namespace Presentation.Controllers
         public PartialViewResult PartialWriterFooter()
         {
             return PartialView();
+        }
+
+        [HttpGet]
+        public IActionResult WriterEditProfile()
+        {
+            var values = writerManager.GetByTId(1);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer writer, string confirmPassword)
+        {
+            WriterValidator validationRules = new WriterValidator();
+            ValidationResult validationResult = validationRules.Validate(writer);
+            if (validationResult.IsValid)
+            {
+                if (writer.WriterPassword == confirmPassword)
+                {
+                    writer.WriterStatus = true;
+                    writer.WriterPassword = BCrypt.Net.BCrypt.HashPassword(writer.WriterPassword);
+                    writerManager.TUpdate(writer);
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    ModelState.AddModelError("WriterPassword", "Girdiğiniz şifreler eşleşmedi. Lütfen tekrar deneyiniz...");
+                }
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
         }
     }
 }
