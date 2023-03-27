1ea1104e8359ed19254b2bc29d15482c4178d91f
Ramazan Onur Abbasoğlu <ramonur26@gmail.com>
2023-03-27 14:31:33 +0300

    Statistic4 added.

---
+++ Presentation/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using DataAccess.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Areas.Admin.ViewComponents.Statistic
+{
+	public class Statistic4:ViewComponent
+	{
+		Context c = new Context();
+
+		public IViewComponentResult Invoke()
+		{
+			ViewBag.v1 = c.Admins.Where(x => x.AdminId == 1).Select(y => y.Name).FirstOrDefault();
+			ViewBag.v2 = c.Admins.Where(x => x.AdminId == 1).Select(y => y.ImageURL).FirstOrDefault();
+			ViewBag.v2 = c.Admins.Where(x => x.AdminId == 1).Select(y => y.Description).FirstOrDefault();
+			return View();
+		}
+	}
+}
+
